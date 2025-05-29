4587f15acc8276d722f8ec119689f07291a5b90b
Q <qontact@protonmail.com>
2025-05-28 20:33:09 -0700

    docs: expand inline comments

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
-  deps = [
+  # Nix environment ensuring TypeScript and VS Code language support on Replit
+  deps = [ # packages made available in the shell
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
