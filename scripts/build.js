e10235d2eddce050aa1b0a409eb8bbf26e212d40
Q <qontact@protonmail.com>
2025-05-30 16:28:21 -0700

    Add SRI hash generation

--- scripts/build.js
+++ scripts/build.js
@@ -11,6 +11,19 @@ function build(){ //runs postcss then renames file to hashed version
   const hash = crypto.createHash('sha1').update(data).digest('hex').slice(0,8); //compute sha1 hash
   fs.renameSync('core.min.css', `core.${hash}.min.css`); //rename with hash
   console.log(`build has run resulting in core.${hash}.min.css`); //log result
+  const shaData = fs.readFileSync(`core.${hash}.min.css`); //read renamed css
+  const sri = crypto.createHash('sha384').update(shaData).digest('base64'); //calc sha384
+  let html = fs.readFileSync('index.html', 'utf8'); //read index
+  html = html.replace(/core\.[a-f0-9]{8}\.min\.css/g, `core.${hash}.min.css`); //update hash
+  html = html.replace(/<link id="cdnCSS"[^>]+>/,
+   `<link id="cdnCSS" rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/Bijikyu/coreCSS/core.${hash}.min.css" integrity="sha384-${sri}" crossorigin="anonymous" onerror="this.onerror=null;this.href='core.${hash}.min.css'">`); //replace cdn link
+  const logoData = fs.readFileSync('core.png'); //read local logo
+  const logoSri = crypto.createHash('sha384').update(logoData).digest('base64'); //calc logo sha
+  html = html.replace(/<link rel="icon"[^>]+>/,
+   `<link rel="icon" type="image/png" href="https://cdn.jsdelivr.net/gh/Bijikyu/staticAssetsSmall/logos/core-logo-min.png" integrity="sha384-${logoSri}" crossorigin="anonymous" onerror="this.onerror=null;this.href='core.png'"/>`); //replace favicon
+  html = html.replace(/<img([^>]+src="https:\/\/cdn\.jsdelivr\.net\/gh\/Bijikyu\/staticAssetsSmall\/logos\/core-logo-min.png"[^>]*)\/>/g,
+   `<img$1 integrity="sha384-${logoSri}" crossorigin="anonymous"/>`); //add sri to logos
+  fs.writeFileSync('index.html', html); //write updated html
   fs.writeFileSync('build.hash', hash); //persist hash
   console.log(`build is returning ${hash}`); //final log before return
   return hash; //return hash
